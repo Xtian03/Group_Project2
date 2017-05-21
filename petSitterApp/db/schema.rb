92ddad0e511049d3089d4d8039042a2b4cdef0ff
Bijay Panthi <bijaypanthi@gmail.com>
2017-05-21 13:09:47 +1000

    User table without service id

--- petSitterApp/db/schema.rb
+++ petSitterApp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520052359) do
+ActiveRecord::Schema.define(version: 20170521025543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,16 @@ ActiveRecord::Schema.define(version: 20170520052359) do
     t.integer  "user_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "password_digest"
+    t.string   "location"
+    t.text     "image"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
