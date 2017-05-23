ca2adfa049662c8e7c2a9a98b008be62f17099fb
matt2c <matt.toussi@gmail.com>
2017-05-23 15:45:42 +1000

    Add Pets button working

--- petSitterApp/app/controllers/pets_controller.rb
+++ petSitterApp/app/controllers/pets_controller.rb
@@ -10,11 +10,12 @@ class PetsController < ApplicationController
   end
 
   def create
-  pet = Pet.new( pet_params )
-  pet.supplier = @current_user
-  pet.save
-  redirect_to pet_path(pet)
- end
+    pet = Pet.new( pet_params )
+    pet.user = @current_user
+    pet.save
+    redirect_to pet_path(pet)
+  end
+
 
   def edit
     @pet = Pet.find_by(id: params['id'])
@@ -35,4 +36,12 @@ class PetsController < ApplicationController
   def pet_params
     params.require(:pet).permit(:name, :date_of_birth, :type_of_pet, :gender, :description, :image)
   end
+
+  def authorise
+  unless @current_user
+    flash[:error] = "You need to be logged first!"
+    redirect_to "/login"
+  end
+end
+
 end
