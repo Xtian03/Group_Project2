f0f03356ab58d30b45c64067262e33fe85607306
Xtian03 <calba03@gmail.com>
2017-05-23 15:00:41 +1000

    Updated

--- petSitterApp/app/controllers/users_controller.rb
+++ petSitterApp/app/controllers/users_controller.rb
@@ -29,7 +29,6 @@ class UsersController < ApplicationController
       redirect_to "/users"
   end
 
-
   def edit
     @user = User.find_by(id: params['id'])
   end
@@ -69,22 +68,3 @@ private
   end
 
 end
-
-
-# Finish the create step --
-  # Define strong params --
-  # Create the user --
-  # Redirect to the list of all users --
-
-# Get into the edit step
-  # Add the form into edit.html.erb (look similar to new.html.erb)
-
-# Get into the update step
-  # Update the user (using user_params - strong params)
-  # Redirect to the profile page of that user
-
-# Get into the destroy step
-  # Button on a page
-  # When someone clicks that
-    # Delete the user
-    # Redirect to the list of all users
